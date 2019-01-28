8a1ce399d6ca9eb319f3805ddaeb551c3cedc5f3
Simon Moll <moll@cs.uni-saarland.de>
2019-01-28 14:23:44 +0100

    wip: predicated insts

--- llvm/include/llvm/IR/PredicatedInst.h
+++ llvm/include/llvm/IR/PredicatedInst.h
@@ -6,13 +6,15 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file defines various classes for working with Instructions and
-// ConstantExprs.
+// This file defines various classes for working with Predicated Instructions.
+// Predicated instructions are either regular instructions or calls to
+// Explicit Vector Length (EVL) intrinsics that have a mask and an explicit
+// vector length argument.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_IR_OPERATOR_H
-#define LLVM_IR_OPERATOR_H
+#ifndef LLVM_IR_PREDICATEDINST_H
+#define LLVM_IR_PREDICATEDINST_H
 
 #include "llvm/ADT/None.h"
 #include "llvm/ADT/Optional.h"
@@ -143,4 +145,92 @@ public:
   void *operator new(size_t s) = delete;
 }
 
-}
+
+
+// PredicatedMatchContext for pattern matching
+struct PredicatedContext {
+  Value * VectorLength;
+  Value * Mask;
+
+  void reset(Value * V) {
+    auto * PredI = dyn_cast<PredicatedInstruction>(V);
+    if (!PredI) {
+      VectorLength = nullptr;
+      Mask = nullptr;
+    }
+    VectorLength = PredI->getVectorLength();
+    Mask = PredI->getMask();
+  }
+
+  PredicatedContext(const PredicatedContext & PC)
+  : Mask (PC.Mask)
+  , VectorLength(PC.VectorLength)
+  {}
+
+  // accept a match where \p Val is in a non-leaf position in a match pattern
+  bool acceptInnerNode(const Value * Val) const {
+    auto PredI = dyn_cast<PredicatedInstruction>(val);
+    if (!PredI) return VectorLength == nullptr && Mask == nullptr;
+    return VectorLength == PredI->getVectorLength() && Mask == PredI->getMask();
+  }
+
+  // accept a match where \p Val is bound to a free variable.
+  bool acceptBoundNode(const Value * Val) const { return true; }
+
+  // whether this context is compatiable with \p E.
+  bool acceptContext(PredicatedContext PC) const {
+    return PC.Mask == Mask && PC.VectorLength == VectorLength;
+  }
+
+  // merge the context \p E into this context and return whether the resulting context is valid.
+  bool mergeContext(PredicatedContext PC) const { return acceptContext(PC); }
+
+  // whether the Value \p Obj behaves like a \p Class.
+  template<typename Class>
+  static bool match_isa(Value* Obj) { return isa<Class>(Obj); }
+  template<> static bool match_isa<BinaryOperator>(Value* Obj) { return isa<PredicatedBinaryOperator>(Obj); }
+  template<> static bool match_isa<Instruction>(Value* Obj)    { return isa<PredicatedInstruction>(Obj); }
+  template<> static bool match_isa<ICmpInst>(Value* Obj)       { return isa<PredicatedICmpInst>(Obj); }
+  template<> static bool match_isa<FCmpInst>(Value* Obj)       { return isa<PredicatedFCmpInst>(Obj); }
+  template<> static bool match_isa<SelectInst>(Value* Obj)     { return isa<PredicatedSelectInst>(Obj); }
+
+  // whether the Value \p Obj behaves like a \p Class.
+  template<typename Class>
+  static auto match_dyn_cast(Value* Obj) { return dyn_cast<Class>(Obj); }
+  template<> static auto match_dyn_cast<BinaryOperator>(Value* Obj) { return dyn_cast<PredicatedBinaryOperator>(Obj); }
+  template<> static auto match_dyn_cast<Instruction>(Value* Obj)    { return dyn_cast<PredicatedInstruction>(Obj); }
+  template<> static auto match_dyn_cast<ICmpInst>(Value* Obj)       { return dyn_cast<PredicatedICmpInst>(Obj); }
+  template<> static auto match_dyn_cast<FCmpInst>(Value* Obj)       { return dyn_cast<PredicatedFCmpInst>(Obj); }
+  template<> static auto match_dyn_cast<SelectInst>(Value* Obj)     { return dyn_cast<PredicatedSelectInst>(Obj); }
+
+  // whether the Value \p Obj behaves like a \p Class.
+  template<typename Class>
+  static auto match_dyn_cast(const Value* Obj) { return dyn_cast<Class>(Obj); }
+  template<> static auto match_dyn_cast<const BinaryOperator>(const Value* Obj) { return dyn_cast<const PredicatedBinaryOperator>(Obj); }
+  template<> static auto match_dyn_cast<const Instruction>(const Value* Obj)    { return dyn_cast<const PredicatedInstruction>(Obj); }
+  template<> static auto match_dyn_cast<const ICmpInst>(const Value* Obj)       { return dyn_cast<const PredicatedICmpInst>(Obj); }
+  template<> static auto match_dyn_cast<const FCmpInst>(const Value* Obj)       { return dyn_cast<const PredicatedFCmpInst>(Obj); }
+  template<> static auto match_dyn_cast<const SelectInst>(const Value* Obj)     { return dyn_cast<const PredicatedSelectInst>(Obj); }
+
+  // whether the Value \p Obj behaves like a \p Class.
+  template<typename Class>
+  static auto match_cast(Value* Obj) { return cast<Class>(Obj); }
+  template<> static auto match_cast<BinaryOperator>(Value* Obj) { return cast<PredicatedBinaryOperator>(Obj); }
+  template<> static auto match_cast<Instruction>(Value* Obj)    { return cast<PredicatedInstruction>(Obj); }
+  template<> static auto match_cast<ICmpInst>(Value* Obj)       { return cast<PredicatedICmpInst>(Obj); }
+  template<> static auto match_cast<FCmpInst>(Value* Obj)       { return cast<PredicatedFCmpInst>(Obj); }
+  template<> static auto match_cast<SelectInst>(Value* Obj)     { return cast<PredicatedSelectInst>(Obj); }
+
+  // whether the Value \p Obj behaves like a \p Class.
+  template<typename Class>
+  static auto match_cast(const Value* Obj) { return cast<Class>(Obj); }
+  template<> static auto match_cast<const BinaryOperator>(const Value* Obj) { return cast<const PredicatedBinaryOperator>(Obj); }
+  template<> static auto match_cast<const Instruction>(const Value* Obj)    { return cast<const PredicatedInstruction>(Obj); }
+  template<> static auto match_cast<const ICmpInst>(const Value* Obj)       { return cast<const PredicatedICmpInst>(Obj); }
+  template<> static auto match_cast<const FCmpInst>(const Value* Obj)       { return cast<const PredicatedFCmpInst>(Obj); }
+  template<> static auto match_cast<const SelectInst>(const Value* Obj)     { return cast<const PredicatedSelectInst>(Obj); }
+};
+
+} // namespace llvm
+
+#endif // LLVM_IR_PREDICATEDINST_H
